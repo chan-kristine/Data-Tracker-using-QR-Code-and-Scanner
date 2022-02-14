f349ebbdf2ffef048d9d6eb748401433d4b6f578
unknown <chankristineagcay@gmail.com>
2022-02-14 15:49:34 +0800

    Working Code: Scans the QR Code with webcam & Records the date and time of capture.

--- qrcodereader.py
+++ qrcodereader.py
@@ -5,37 +5,38 @@
 #	- You may decide which personal data to include
 #	- All data read from QRCode should be stored in a text file including the date and time it was read
 
-# Create QR Code
 
-import qrcode
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-link = "https://web.facebook.com/Luunnnaaa/"
-qr.add_data(link)
-qr.make(fit=True)
-img = qr.make_image(fill_color="black", back_color="pink")
-img.save("qrcodechan.png")
-
-
-# QR Code Scanner
-import cv2
-import webbrowser
+import cv2 
+import numpy as np
 import datetime
-vcapture = cv2.VideoCapture(0)
-vdetector = cv2.QRCodeDetector()
-while True:
-    _, img = vcapture.read()
-    data, one, _ = vdetector.detectAndDecode(img)
-    if data:
-        read = data
-        break
-    cv2.imshow('QRCode Scanner', img)
-    if cv2.waitKey(1)==ord('a'):
-        break
-redirect = webbrowser.open((str(read)))
-vcapture.release(read)
-cv2.destroyAllWindows
+from pyzbar.pyzbar import decode 
+
+CamCapture = cv2.VideoCapture(0)
+CamCapture.set(3,1080) #width
+CamCapture.set(4,720) #height
+
+CamScanner = True
+while CamScanner == True:
+    success, frame = CamCapture.read()
+
+    for captureinfomartions in decode(frame):
+        #Convert infos to text file
+        txt_file = open("Data.txt", "w")
+        txt_file.write(f"{captureinfomartions.data.decode('utf-8')}\n" )
+        
+        #Add the time and date when data is scanned
+        Date = datetime.datetime.now()
+        txt_file.write(Date.strftime("Date: %m/%d/%y \n"))
+        txt_file.write(Date.strftime("Time: %H:%M:%S"))  
+        txt_file.close()
+
+        #Add effects for the program
+        Filename = "SCANNING COMPLETED!"
+        Qr_code_detector = np.array([captureinfomartions.polygon],np.int32)
+        Qr_code_detector = Qr_code_detector.reshape((-1,1,2))
+        cv2.polylines(frame, [Qr_code_detector], True, (0,128,0), 5)
+        Qr_code_detector_2 = captureinfomartions.rect
+        cv2.putText(frame, Filename, (Qr_code_detector_2 [0], Qr_code_detector_2[1]), cv2.FONT_HERSHEY_DUPLEX, 0.9,(0,128,0), 2)
+               
+    cv2.imshow('Aleja QR Code Scanner', frame)
+    cv2.waitKey(1)
